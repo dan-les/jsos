548eb496a173ce65d0edf8ccca2dbe576bca141c
rootuss <rootuss@op.pl>
2019-06-19 13:24:16 +0200

    ściaśnienie kodu

--- srednia.js
+++ srednia.js
@@ -1,31 +1,7 @@
-$('td:contains("2.0")').parent().remove(); //usunięcie wpisów z oceną 2
-$('td:contains("---")').parent().remove(); //usunięcie ocen nie wpisanych, aby ich nie liczyło
-$('td:contains("k.cz.")').parent().remove(); //usunięcie ocen cząstkowych
-
-var sum=0; //suma ECTS
-var mulSum=0; // suma ECTS * ocena
-$('td[title="ocena"]').each(function(){
-    mulSum += parseFloat($(this).text()) * parseFloat($(this).next().next().text());
-    sum += parseFloat($(this).next().next().text());
-    console.log(parseFloat($(this).text()), parseFloat($(this).next().next().text()));
-    
-});
-
-Math.decimal = function(n, k) 
-{
-    var factor = Math.pow(10, k+1);
-    n = Math.round(Math.round(n*factor)/10);
-    return n/(factor/10);
-}
-console.log(mulSum, sum, mulSum/sum);
-var sum=0; //suma ECTS 
-var mulSum=0; // suma ECTS * ocena 
-$('td[title="ocena"]').each(function(){
-    mulSum += parseFloat($(this).text()) * parseFloat($(this).next().next().text());
-    sum += parseFloat($(this).next().next().text());
-    console.log(parseFloat($(this).text()), parseFloat($(this).next().next().text()));
-});
-clear();
+$('td:contains("2.0")').parent().remove();$('td:contains("---")').parent().remove();$('td:contains("k.cz.")').parent().remove();
+var sum=0;var mulSum=0;$('td[title="ocena"]').each(function(){mulSum += parseFloat($(this).text()) * parseFloat($(this).next().next().text());sum += parseFloat($(this).next().next().text());console.log(parseFloat($(this).text()), parseFloat($(this).next().next().text()));});
+Math.decimal = function(n, k) {var factor = Math.pow(10, k+1);n = Math.round(Math.round(n*factor)/10);return n/(factor/10);}
+console.log(mulSum, sum, mulSum/sum);var sum=0;var mulSum=0; $('td[title="ocena"]').each(function(){mulSum += parseFloat($(this).text()) * parseFloat($(this).next().next().text());sum += parseFloat($(this).next().next().text());console.log(parseFloat($(this).text()), parseFloat($(this).next().next().text()));});clear();
 console.log("KALKULATOR LICZĄCY ŚREDNIĄ Z WPISANYCH OCEN DO JSOS'a (nie liczy do średniej ndst)");
 console.log("(nie liczy do średniej ndst)");
 console.log("##########################################"); 

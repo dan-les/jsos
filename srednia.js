bc1c903273035f50464b1d6d846323cbfb9b4a9a
Daniel <rootuss@op.pl>
2020-07-03 08:52:38 +0200

    Update srednia.js

--- srednia.js
+++ srednia.js
@@ -2,7 +2,7 @@ $('td:contains("2.0")').parent().remove();$('td:contains("---")').parent().remov
 var sum=0;var mulSum=0;$('td[title="ocena"]').each(function(){mulSum += parseFloat($(this).text()) * parseFloat($(this).next().next().text());sum += parseFloat($(this).next().next().text());console.log(parseFloat($(this).text()), parseFloat($(this).next().next().text()));});
 Math.decimal = function(n, k) {var factor = Math.pow(10, k+1);n = Math.round(Math.round(n*factor)/10);return n/(factor/10);}
 console.log(mulSum, sum, mulSum/sum);var sum=0;var mulSum=0; $('td[title="ocena"]').each(function(){mulSum += parseFloat($(this).text()) * parseFloat($(this).next().next().text());sum += parseFloat($(this).next().next().text());console.log(parseFloat($(this).text()), parseFloat($(this).next().next().text()));});clear();
-console.log("KALKULATOR LICZĄCY ŚREDNIĄ Z WPISANYCH OCEN DO JSOS'a (nie liczy do średniej ndst)");
+console.log("KALKULATOR LICZĄCY ŚREDNIĄ Z WPISANYCH OCEN DO JSOS'a");
 console.log("(nie liczy do średniej ndst)");
 console.log("##########################################"); 
 console.log("Wymnożone ECTS'y razy oceny:       " + mulSum);

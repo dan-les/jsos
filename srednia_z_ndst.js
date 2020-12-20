0f39449e7d8e28e331305b72804b23b07efcab9c
Daniel Leśniewicz <rootuss@op.pl>
2020-12-20 10:36:31 +0100

    Update srednia_z_ndst.js

--- srednia_z_ndst.js
+++ srednia_z_ndst.js
@@ -4,7 +4,7 @@ Math.decimal = function(n, k) {var factor = Math.pow(10, k+1);n = Math.round(Mat
 console.log(mulSum, sum, mulSum/sum);var sum=0;var mulSum=0; $('td[title="ocena"]').each(function(){mulSum += parseFloat($(this).text()) * parseFloat($(this).next().next().text());sum += parseFloat($(this).next().next().text());console.log(parseFloat($(this).text()), parseFloat($(this).next().next().text()));});clear();
 //console.log("############################################");
 console.log("KALKULATOR LICZĄCY ŚREDNIĄ Z WPISANYCH OCEN DO JSOS'a");
-console.log("(nie liczy do średniej ndst)"); 
+console.log("(liczy do średniej ndst)"); 
 console.log("");
 console.log("   Wymnożone ECTS'y razy oceny:     " + mulSum);
 console.log("    Suma ECTS'ów z tego okresu:     "+ sum);
